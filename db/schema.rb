f3e33645245613a4bf955c48eef1f3db5a481192
Andrianka <andrianka13@gmail.com>
2014-11-16 23:29:47 +0100

    added migration to change the  type of a column price

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115193533) do
+ActiveRecord::Schema.define(version: 20141116221907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20141115193533) do
   create_table "products", force: true do |t|
     t.integer  "product_key"
     t.string   "name"
-    t.integer  "price"
+    t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
