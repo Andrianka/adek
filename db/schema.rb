b1de13b6f97c15d33fbc7b5cb1ff7a0c517c9129
Andrianka <andrianka13@gmail.com>
2014-11-17 19:21:46 +0100

    added migration - price to order items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141116221907) do
+ActiveRecord::Schema.define(version: 20141117181257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20141116221907) do
     t.integer  "quantity"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "price"
   end
 
   create_table "orders", force: true do |t|
